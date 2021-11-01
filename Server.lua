1bc3c596297d5d12cfbb225f136f3a7fc7682ec5
Bombay <OMGmsebastian@gmail.com>
2021-11-01 17:04:42 -0400

    feat(server): change command to server side

--- Server.lua
+++ Server.lua
@@ -1,8 +1,9 @@
 local token = "PUT_YOUR_TOKEN"  ---PUT YOUR TOKEN
 local FormattedToken = "Bot " ..token
 
+local ESX = exports['es_extended']:getSharedObject()
 
-function GetDiscordName(user) 
+local function GetDiscordName(user)
     local discordId = nil
     local DiscordName = nil;
     for _, id in ipairs(GetPlayerIdentifiers(user)) do
@@ -11,22 +12,22 @@ function GetDiscordName(user)
             break
         end
     end
-    if discordId then 
+    if discordId then
         local endpoint = ("users/%s"):format(discordId)
         local member = GetInfoFromDiscord("GET", endpoint, {})
         if member.code == 200 then
             local data = json.decode(member.data)
-            if data ~= nil then 
+            if data ~= nil then
                 DiscordName = data.username .. "#" .. data.discriminator;
             end
-        else 
+        else
         	print("An error ocurred.")
         end
     end
-    return DiscordName;
+    return DiscordName
 end
 
-function GetAvatar(user) 
+local function GetAvatar(user)
     local discordId = nil
     local DiscordImg = nil;
     for _, id in ipairs(GetPlayerIdentifiers(user)) do
@@ -36,27 +37,96 @@ function GetAvatar(user)
         end
 	end
 	if discordId then 
-			local endpoint = ("users/%s"):format(discordId)
-			local member = GetInfoFromDiscord("GET", endpoint, {})
-			if member.code == 200 then
-				local data = json.decode(member.data)
-				if data ~= nil and data.avatar ~= nil then 
-					if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then 
-						DiscordImg = "https://cdn.discordapp.com/avatars/" .. discordId .. "/" .. data.avatar .. ".gif?";
-					else 
-						DiscordImg = "https://cdn.discordapp.com/avatars/" .. discordId .. "/" .. data.avatar .. ".png?"
-					end
-				end
-			else 
-				print("An error ocurred.")
-			end
+        local endpoint = ("users/%s"):format(discordId)
+        local member = GetInfoFromDiscord("GET", endpoint, {})
+        if member.code == 200 then
+            local data = json.decode(member.data)
+            if data ~= nil and data.avatar ~= nil then 
+                if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then 
+                    DiscordImg = "https://cdn.discordapp.com/avatars/" .. discordId .. "/" .. data.avatar .. ".gif?";
+                else 
+                    DiscordImg = "https://cdn.discordapp.com/avatars/" .. discordId .. "/" .. data.avatar .. ".png?"
+                end
+            end
+        else 
+            print("An error ocurred.")
+        end
 	else 
 		print("The player don't have discord.")
 	end
     return DiscordImg;
 end
 
+local function getBanner(user)
+    local discordId = nil
+    local bannerImg = nil
+    for _, id in ipairs(GetPlayerIdentifiers(user)) do
+        if string.match(id, "discord:") then
+            discordId = string.gsub(id, "discord:", "")
+            break
+        end
+	end
+	if discordId then
+        local endpoint = ("users/%s"):format(discordId)
+        local member = GetInfoFromDiscord("GET", endpoint, {})
+        if member.code == 200 then
+            local data = json.decode(member.data)
+            if data ~= nil and data.avatar ~= nil then 
+                if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then 
+                    bannerImg = "https://cdn.discordapp.com/banners/" .. discordId .. "/" .. data.banner .. ".gif?size=1024";
+                else
+                    bannerImg = "https://cdn.discordapp.com/banners/" .. discordId .. "/" .. data.banner .. ".png?size=1024"
+                end
+            end
+        else
+            print("An error ocurred.")
+        end
+	else
+		print("The player don't have discord.")
+	end
+    return bannerImg
+end
 
+local function GetInfoFromDiscord(method, endpoint, jsondata)
+    local data = nil
+    PerformHttpRequest("https://discordapp.com/api/"..endpoint, function(errorCode, resultData, resultHeaders)
+		data = {data=resultData, code=errorCode, headers=resultHeaders}
+    end, method, #jsondata > 0 and json.encode(jsondata) or "", {["Content-Type"] = "application/json", ["Authorization"] = FormattedToken})
+
+    while data == nil do
+        Citizen.Wait(0)
+    end
+
+    return data
+end
+
+RegisterCommand(Config.CommandPanel, function(source)
+    local playerId <const> = source
+    local xPlayer = ESX.GetPlayerFromId(playerId)
+    if xPlayer then
+        local data = {
+            jobs = {
+                police = #ESX.GetExtendedPlayers('job', 'police') or 0,
+                ems = #ESX.GetExtendedPlayers('job', 'ambulance') or 0,
+                mechanic = #ESX.GetExtendedPlayers('job', 'mechanic') or 0
+            },
+            accounts = {
+                money = xPlayer.getAccount(Config.MoneyAccount).money or 0,
+                bank = xPlayer.getAccount(Config.BankAccount).money or 0,
+                black = xPlayer.getAccount(Config.BlackAccount).money or 0
+            },
+            playerData = {
+                banner = getBanner(src) or Config.DefaultBanner,
+                avatar = Config.DefaultAvatar,
+                job = xPlayer.getJob(),
+                players = GetNumPlayerIndices() or 0,
+                maxPlayers = GetCon
+            }
+        }
+
+        TriggerClientEvent('Roda_InfoPanel', playerId, data)
+    end
+end)
 
 RegisterNetEvent('Roda_InfoPanel:FromServer')
 AddEventHandler('Roda_InfoPanel:FromServer', function()
@@ -66,9 +136,6 @@ AddEventHandler('Roda_InfoPanel:FromServer', function()
 	local banner = getBanner(src)
     local ava = GetAvatar(src)
     local job = xPlayer.getJob()
-    local money = xPlayer.getAccount(Config.MoneyAccount).money
-    local bank = xPlayer.getAccount(Config.BankAccount).money
-    local black = xPlayer.getAccount(Config.BlackAccount).money
 
     local connectdPlayers = GetNumPlayerIndices()
 
@@ -82,82 +149,4 @@ AddEventHandler('Roda_InfoPanel:FromServer', function()
     elseif not banner and not ava then 
         TriggerClientEvent('Roda_InfoPanel', src, Config.DefaultBanner, Config.DefaultAvatar, accountsmoney, job, connectdPlayers)
     end
-end)
-
-
-
-function getBanner(user) 
-    local discordId = nil
-    local bannerImg = nil;
-    for _, id in ipairs(GetPlayerIdentifiers(user)) do
-        if string.match(id, "discord:") then
-            discordId = string.gsub(id, "discord:", "")
-            break
-        end
-	end
-	if discordId then 
-			local endpoint = ("users/%s"):format(discordId)
-			local member = GetInfoFromDiscord("GET", endpoint, {})
-			if member.code == 200 then
-				local data = json.decode(member.data)
-				if data ~= nil and data.avatar ~= nil then 
-					if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then 
-						bannerImg = "https://cdn.discordapp.com/banners/" .. discordId .. "/" .. data.banner .. ".gif?size=1024";
-					else 
-						bannerImg = "https://cdn.discordapp.com/banners/" .. discordId .. "/" .. data.banner .. ".png?size=1024"
-					end
-				end
-			else 
-				print("An error ocurred.")
-			end
-	else 
-		print("The player don't have discord.")
-	end
-    return bannerImg;
-end
-
-
-
-
-function GetInfoFromDiscord(method, endpoint, jsondata)
-    local data = nil
-    PerformHttpRequest("https://discordapp.com/api/"..endpoint, function(errorCode, resultData, resultHeaders)
-		data = {data=resultData, code=errorCode, headers=resultHeaders}
-    end, method, #jsondata > 0 and json.encode(jsondata) or "", {["Content-Type"] = "application/json", ["Authorization"] = FormattedToken})
-
-    while data == nil do
-        Citizen.Wait(0)
-    end
-	
-    return data
-end
-
-
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterServerCallback('Roda_InfoPanel:GetJobCount', function(source, cb)
-
-    local _police = exports['playertables']:getPlayers("police") 
-    local _ems = exports['playertables']:getPlayers("ambulance") 
-    local _mechanic = exports['playertables']:getPlayers("mechanic")
-    EMSConnected = 0
-	PoliceConnected = 0
-	MechanicConnected = 0
-
-    for k, v in pairs(_police) do -- Iterate table of players with job
-        PoliceConnected = PoliceConnected + 1
-    end
-
-    for k, v in pairs(_mechanic) do -- Iterate table of players with job
-        MechanicConnected = MechanicConnected + 1
-    end
-
-    for k, v in pairs(_ems) do -- Iterate table of players with job
-        EMSConnected = EMSConnected + 1
-    end
-
-  cb(EMSConnected, PoliceConnected, MechanicConnected)
-  
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,18 +1,17 @@
 fx_version 'cerulean'
 
-
 game 'gta5'
+
+lua54 'yes'
+use_fxv2_oal 'yes'
 
 author 'Roderic#0614'
 description 'Roda Info Panel'
 
-client_scripts {
-    'Client.lua'
-}
-
-shared_script 'Config.lua'
+client_script 'Client.lua'
 
 server_scripts {
+    'Config.lua',
     'Server.lua'
 }
 
