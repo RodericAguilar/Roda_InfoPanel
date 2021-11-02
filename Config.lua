298d1a929d3bccca74fd7c723bdb7c0851faf2a1
BombayV <OMGmsebastian@gmail.com>
2021-11-02 10:15:57 -0400

    feat(server/js): added config for fade animations

--- Config.lua
+++ Config.lua
@@ -1,17 +1,15 @@
 Config = {}
 
-Config.DefaultBanner = 'https://wallpaperaccess.com/full/1615355.jpg'  --If the person don't have banner, this picture is displayed.
-Config.DefaultAvatar = 'https://i.pinimg.com/736x/8b/41/c5/8b41c5d25078fe59db85e8eaf251833f.jpg' --If the person don't have avatar, this picture is displayed
+Config.DefaultBanner = 'https://wallpaperaccess.com/full/1615355.jpg'  --If the person don't have banner, this picture is displayed (occurs if discord is not open).
+Config.DefaultAvatar = 'https://i.pinimg.com/736x/8b/41/c5/8b41c5d25078fe59db85e8eaf251833f.jpg' --If the person don't have avatar, this picture is displayed (occurs if discord is not open).
+Config.FadeIn = 1000 -- Fade In animation duration.
+Config.FadeOut = 4000 -- Fade Out animation duration.
 
 --Money Accounts--
-Config.MoneyAccount = 'money' --money account
-Config.BankAccount = 'bank' --bank account
-Config.BlackAccount = 'black_money'  --Black money account
+Config.MoneyAccount = 'money' --money account.
+Config.BankAccount = 'bank' --bank account.
+Config.BlackAccount = 'black_money'  --Black money account.
 
-Config.MaxSvPlayers = 1024  --Put how many slots you want to display in the panel.
+Config.MaxSvPlayers = 32  -- Not necessary. It will automatically find your max players but just in case it doesn't work.
 
-Config.CommandPanel = 'showInfo' -- Command for open de panel
-
----Key for open--
-Config.UseKey = false -- Put true if u want that the menu can open with a key
-Config.OpenPanel = 'F4'  --You can change before start the server or also in game all players can change his key.  -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.CommandPanel = 'showInfo' -- Command for opening the panel.
--- Server.lua
+++ Server.lua
@@ -3,28 +3,17 @@ local FormattedToken = "Bot " ..token
 
 local ESX = exports['es_extended']:getSharedObject()
 
-local function GetDiscordName(user)
-    local discordId = nil
-    local DiscordName = nil;
-    for _, id in ipairs(GetPlayerIdentifiers(user)) do
-        if string.match(id, "discord:") then
-            discordId = string.gsub(id, "discord:", "")
-            break
-        end
+local function GetInfoFromDiscord(method, endpoint, jsondata)
+    local data = nil
+    PerformHttpRequest("https://discordapp.com/api/"..endpoint, function(errorCode, resultData, resultHeaders)
+		data = {data=resultData, code=errorCode, headers=resultHeaders}
+    end, method, #jsondata > 0 and json.encode(jsondata) or "", {["Content-Type"] = "application/json", ["Authorization"] = FormattedToken})
+
+    while data == nil do
+        Wait(0)
     end
-    if discordId then
-        local endpoint = ("users/%s"):format(discordId)
-        local member = GetInfoFromDiscord("GET", endpoint, {})
-        if member.code == 200 then
-            local data = json.decode(member.data)
-            if data ~= nil then
-                DiscordName = data.username .. "#" .. data.discriminator;
-            end
-        else
-        	print("An error ocurred.")
-        end
-    end
-    return DiscordName
+
+    return data
 end
 
 local function GetAvatar(user)
@@ -36,25 +25,25 @@ local function GetAvatar(user)
             break
         end
 	end
-	if discordId then 
+	if discordId then
         local endpoint = ("users/%s"):format(discordId)
         local member = GetInfoFromDiscord("GET", endpoint, {})
         if member.code == 200 then
             local data = json.decode(member.data)
-            if data ~= nil and data.avatar ~= nil then 
-                if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then 
+            if data ~= nil and data.avatar ~= nil then
+                if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then
                     DiscordImg = "https://cdn.discordapp.com/avatars/" .. discordId .. "/" .. data.avatar .. ".gif?";
-                else 
+                else
                     DiscordImg = "https://cdn.discordapp.com/avatars/" .. discordId .. "/" .. data.avatar .. ".png?"
                 end
             end
-        else 
+        else
             print("An error ocurred.")
         end
-	else 
+	else
 		print("The player don't have discord.")
 	end
-    return DiscordImg;
+    return DiscordImg
 end
 
 local function getBanner(user)
@@ -71,8 +60,8 @@ local function getBanner(user)
         local member = GetInfoFromDiscord("GET", endpoint, {})
         if member.code == 200 then
             local data = json.decode(member.data)
-            if data ~= nil and data.avatar ~= nil then 
-                if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then 
+            if data ~= nil and data.avatar ~= nil then
+                if (data.avatar:sub(1, 1) and data.avatar:sub(2, 2) == "_") then
                     bannerImg = "https://cdn.discordapp.com/banners/" .. discordId .. "/" .. data.banner .. ".gif?size=1024";
                 else
                     bannerImg = "https://cdn.discordapp.com/banners/" .. discordId .. "/" .. data.banner .. ".png?size=1024"
@@ -87,66 +76,38 @@ local function getBanner(user)
     return bannerImg
 end
 
-local function GetInfoFromDiscord(method, endpoint, jsondata)
-    local data = nil
-    PerformHttpRequest("https://discordapp.com/api/"..endpoint, function(errorCode, resultData, resultHeaders)
-		data = {data=resultData, code=errorCode, headers=resultHeaders}
-    end, method, #jsondata > 0 and json.encode(jsondata) or "", {["Content-Type"] = "application/json", ["Authorization"] = FormattedToken})
-
-    while data == nil do
-        Citizen.Wait(0)
-    end
-
-    return data
-end
-
 RegisterCommand(Config.CommandPanel, function(source)
     local playerId <const> = source
     local xPlayer = ESX.GetPlayerFromId(playerId)
-    if xPlayer then
-        local data = {
-            jobs = {
-                police = #ESX.GetExtendedPlayers('job', 'police') or 0,
-                ems = #ESX.GetExtendedPlayers('job', 'ambulance') or 0,
-                mechanic = #ESX.GetExtendedPlayers('job', 'mechanic') or 0
-            },
-            accounts = {
-                money = xPlayer.getAccount(Config.MoneyAccount).money or 0,
-                bank = xPlayer.getAccount(Config.BankAccount).money or 0,
-                black = xPlayer.getAccount(Config.BlackAccount).money or 0
-            },
-            playerData = {
-                banner = getBanner(src) or Config.DefaultBanner,
-                avatar = Config.DefaultAvatar,
-                job = xPlayer.getJob(),
-                players = GetNumPlayerIndices() or 0,
-                maxPlayers = GetCon
+    if playerId > 0 then
+        if xPlayer then
+            local data <const> = {
+                jobs = {
+                    police = #ESX.GetExtendedPlayers('job', 'police') or 0,
+                    ems = #ESX.GetExtendedPlayers('job', 'ambulance') or 0,
+                    mechanic = #ESX.GetExtendedPlayers('job', 'mechanic') or 0
+                },
+                accounts = {
+                    money = xPlayer.getAccount(Config.MoneyAccount).money or 0,
+                    bank = xPlayer.getAccount(Config.BankAccount).money or 0,
+                    black = xPlayer.getAccount(Config.BlackAccount).money or 0
+                },
+                playerData = {
+                    banner = getBanner(playerId) or Config.DefaultBanner,
+                    avatar = GetAvatar(playerId) or Config.DefaultAvatar,
+                    job = xPlayer.getJob(),
+                    name = GetPlayerName(playerId),
+                    id = playerId,
+                    players = GetNumPlayerIndices() or 1,
+                    maxPlayers = GetConvar("sv_maxclients", "32") or Config.MaxSvPlayers,
+                    fadeIn = Config.FadeIn or 1000,
+                    fadeOut = Config.FadeOut or 4000
+                }
             }
-        }
-
-        TriggerClientEvent('Roda_InfoPanel', playerId, data)
-    end
-end)
-
-RegisterNetEvent('Roda_InfoPanel:FromServer')
-AddEventHandler('Roda_InfoPanel:FromServer', function()
-    local accountsmoney = {}
-    local src = source
-    local xPlayer = ESX.GetPlayerFromId(src)
-	local banner = getBanner(src)
-    local ava = GetAvatar(src)
-    local job = xPlayer.getJob()
-
-    local connectdPlayers = GetNumPlayerIndices()
-
-    accountsmoney = {money = money,bank = bank,black = black}
-    if banner and ava then 
-        TriggerClientEvent('Roda_InfoPanel', src, banner, ava, accountsmoney, job, connectdPlayers)
-    elseif banner and not ava then 
-        TriggerClientEvent('Roda_InfoPanel', src, banner, Config.DefaultAvatar, accountsmoney, job, connectdPlayers)
-    elseif ava and not banner then 
-        TriggerClientEvent('Roda_InfoPanel', src, Config.DefaultBanner, ava, accountsmoney, job, connectdPlayers)
-    elseif not banner and not ava then 
-        TriggerClientEvent('Roda_InfoPanel', src, Config.DefaultBanner, Config.DefaultAvatar, accountsmoney, job, connectdPlayers)
+            if type(data) ~="table" then
+                return print('Something went wrong')
+            end
+            TriggerClientEvent('Roda_InfoPanel', playerId, data)
+        end
     end
 end)
